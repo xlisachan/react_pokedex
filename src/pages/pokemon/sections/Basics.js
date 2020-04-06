9cc8c2d1efc0c775cf6a082a12cae1c0b28cc1f5
xlisachan <xlisachan@gmail.com>
2020-04-06 14:16:31 -0400

    Added ability to add and remove pokemon

--- src/pages/pokemon/sections/Basics.js
+++ src/pages/pokemon/sections/Basics.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import '../Pokemon.scss';
+import { connect } from 'react-redux';
+import { addPokemon, removePokemon } from '../../../actions/trainerActions';
+import DeleteModal from '../../../components/common/DeleteModal';
 
-const Basics = ({ pokemon }) => {
-  const displayType = (type) => {
+class Basics extends React.Component {
+  state = {
+    isOwned: null,
+  }
+
+  componentDidMount() {
+    this.hasRelation(this.props.pokemon.id)
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.pokedex.length !== prevProps.pokedex.length) {
+      this.hasRelation(this.props.pokemon.id)
+    }
+  }
+
+  hasRelation = id => {
+    const rel = this.props.pokedex.filter(mon => mon.id === id);
+    this.setState({ isOwned: rel.length > 0 ? true : false });
+  }
+
+  deleteButton =
+    <DeleteModal
+      item={'pokemon'}
+      el={this.props.pokemon}
+      onClick={() => this.props.onRemovePokemon(this.props.trainer.name, this.props.pokemon.id)}
+    />
+
+  addButton =
+    <div
+      className="addpokemon-button"
+      onClick={() => this.props.onAddPokemon(this.props.trainer.name, {
+        img: this.props.pokemon.image,
+        id: this.props.pokemon.id,
+        name: this.props.pokemon.name
+      })
+      }
+    >
+      Add To Pokedex
+    </div>
+
+  displayType = (type) => {
     switch (type) {
       case 'bug':
         return 'darkseagreen';
@@ -43,25 +85,52 @@ const Basics = ({ pokemon }) => {
       default:
         return 'black'
     }
+  };
+
+  render() {
+    const { pokemon } = this.props;
+    return (
+      <section>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img src={pokemon.image} alt={pokemon.name} />
+
+          <div>
+            <p>{pokemon.name}</p>
+
+            <p style={{ margin: '5px 0' }}>
+              <span
+                className="pokemon-type"
+                style={{ background: this.displayType(pokemon.type) }}
+              >
+                {pokemon.type}
+              </span>
+            </p>
+          </div>
+        </div>
+
+        {this.state.isOwned ? this.deleteButton : this.addButton}
+      </section>
+    );
+  };
+};
+
+const mapStateToProps = state => ({
+  trainer: state.trainer,
+  pokedex: state.trainer.pokedex
+});
+
+const mapDispatchToProps = dispatch => ({
+  onAddPokemon(trainerName, newPokemon) {
+    dispatch(
+      addPokemon(trainerName, newPokemon)
+    )
+  },
+
+  onRemovePokemon(trainerName, pokemonId) {
+    dispatch(
+      removePokemon(trainerName, pokemonId)
+    )
   }
-  return (
-    <section style={{ display: 'flex', alignItems: 'center' }}>
-      <img src={pokemon.image} alt={pokemon.name} />
+});
 
-      <div>
-        <p>{pokemon.name}</p>
-
-        <p style={{marginTop: 5}}>
-          <span
-            className="pokemon-type"
-            style={{ background: displayType(pokemon.type) }}
-          >
-            {pokemon.type}
-          </span>
-        </p>
-      </div>
-    </section>
-  );
-}
-
-export default Basics;
+export default connect(mapStateToProps, mapDispatchToProps)(Basics);
