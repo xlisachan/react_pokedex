7f635d5d8541ec1d16dc16d9c0a67cc1860bcaae
xlisachan <xlisachan@gmail.com>
2020-04-05 22:25:53 -0400

    Updated Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { pokeSearch } from '../actions/searchActions';
 import { FaSearch } from 'react-icons/fa';
 
-const Search = () => (
-  <form className="search-form" autoComplete="off">
-    <FaSearch style={{ fontSize: '1rem', marginRight: 10 }} />
-          
-    <input className="input-search" type="text" placeholder="Enter Pokémon" />
-  </form>
-);
+const Search = ({history, onSearchPokemon=f=>f}) => {
+  const [query, setQuery] = React.useState('');
+    let searchEl = React.useRef();
 
-export default Search;
+    const handleChange = () => {
+      setQuery(searchEl.value);
+    }
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    let newQuery;
+    typeof query === 'string' ? newQuery = query.toLowerCase() : newQuery = query;
+    onSearchPokemon(newQuery, history);
+    setQuery('');
+  }
+  return (
+    <form style={{ display: 'flex', alignItems: 'center' }} className="search-form" autoComplete="off" onSubmit={onSubmit}>
+      <FaSearch style={{ fontSize: '1rem', marginRight: 10 }} />
+
+      <input
+        ref={input => searchEl = input}
+        className="input-search"
+        type="text"
+        placeholder="Enter Pokémon"
+        value={query}
+        onChange={handleChange}
+      />
+    </form>
+  )
+};
+
+const mapDispatchToProps = dispatch => ({
+  onSearchPokemon(pokemon, history) {
+    dispatch(
+      pokeSearch(pokemon, history)
+    )
+  }
+});
+
+export default connect(null, mapDispatchToProps)(withRouter(Search));
