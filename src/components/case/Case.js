bd8c9f0a055f3cc011d5d4254d4940f1a7f36ad8
xlisachan <xlisachan@gmail.com>
2020-04-06 01:27:59 -0400

    Added event listener for case

--- src/components/case/Case.js
+++ src/components/case/Case.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './Case.scss';
 import Main from './Main';
+import start from '../../assets/audio/start.mp4';
 
-const Case = () => (
-  <div className="wrapper">
-    <div className="case case-top">
-      <div className="circle-black top-outer">
-        <div className="circle-blue top-inner animated infinite pulse"></div>
+const Case = () => {
+  const topEl = useRef(),
+        botEl = useRef(),
+        topButton = useRef(),
+        botButton = useRef(),
+        audio = new Audio(start);
+  
+  const handleClick = () => {
+    return topEl.current.style.top === '24vh' ?
+      (
+        topEl.current.style.top = '0vh',
+        botEl.current.style.top = '0vh',
+        topButton.current.classList.remove('infinite'),
+        botButton.current.classList.remove('infinite'),
+        audio.play()
+      )
+      :
+      (
+        topEl.current.style.top = '24vh',
+        botEl.current.style.top = '-24vh',
+        topButton.current.classList.add('infinite'),
+        botButton.current.classList.add('infinite')
+      );
+  }
+  
+  return (
+    <div className="wrapper">
+      <div ref={topEl} className="case case-top" style={{top: "24vh"}}>
+        <div className="circle-black top-outer">
+          <div ref={topButton} className="circle-blue top-inner animated infinite pulse" onClick={handleClick}></div>
+        </div>
+      </div>
+      
+      <Main />
+      
+      <div ref={botEl} className="case case-bottom" style={{top: "-24vh"}}>
+        <div className="circle-black bottom-outer">
+          <div ref={botButton} className="circle-blue bottom-inner animated infinite pulse" onClick={handleClick}></div>
+        </div>
       </div>
     </div>
-    
-    <Main />
-    
-    <div className="case case-bottom">
-      <div className="circle-black bottom-outer">
-        <div className="circle-blue bottom-inner animated infinite pulse"></div>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Case;
