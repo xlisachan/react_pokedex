a96ae0a80ec3995a5a6da97add1554dcba958188
xlisachan <xlisachan@gmail.com>
2020-04-06 11:53:57 -0400

    Utilize MUI to create menu button

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './Navbar.scss';
 import { withRouter } from 'react-router-dom';
 import Search from '../Search';
+import NavButton from './NavButton';
 
 const NavBar = ({ location }) => (
   <nav className="navbar-container">
     <div className="navbar">
-      <div>
+      <div style={{width: "80%"}}>
         {location.pathname === "/" ? null : <Search />}
       </div>
 
+      {location.pathname === "/trainer/lisa" ? null : <NavButton />}
     </div>
   </nav>
 );
