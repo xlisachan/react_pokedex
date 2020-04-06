f53325150c85f4d4d9bf3714c7603b12f85d028f
xlisachan <xlisachan@gmail.com>
2020-04-06 14:57:26 -0400

    Updates

---
+++ precache-manifest.09a74ca16751d7cd026958b15ff475e5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1dbd5502f1aba1db47dcea93145df63",
+    "url": "/react_pokedex/index.html"
+  },
+  {
+    "revision": "87933c923714724c402c",
+    "url": "/react_pokedex/static/css/main.16530453.chunk.css"
+  },
+  {
+    "revision": "165b6bea83578b661fa5",
+    "url": "/react_pokedex/static/js/2.438ba8f6.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/react_pokedex/static/js/2.438ba8f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87933c923714724c402c",
+    "url": "/react_pokedex/static/js/main.17fee98b.chunk.js"
+  },
+  {
+    "revision": "870958e7759b77ae1d11",
+    "url": "/react_pokedex/static/js/runtime-main.0dc314ef.js"
+  },
+  {
+    "revision": "73222bbc6311e026c24357a5e63ac328",
+    "url": "/react_pokedex/static/media/detectivepikachu.73222bbc.png"
+  },
+  {
+    "revision": "02351b5da5bf3666615c497382c2eae8",
+    "url": "/react_pokedex/static/media/loading.02351b5d.gif"
+  },
+  {
+    "revision": "f7b41b0b170d8765d7f6684497c7763a",
+    "url": "/react_pokedex/static/media/pokeball.f7b41b0b.png"
+  },
+  {
+    "revision": "bdcd20f5411ee5785889542d303ad4cb",
+    "url": "/react_pokedex/static/media/pokedex.bdcd20f5.png"
+  },
+  {
+    "revision": "6b36186fe41fa0a497d5ddc29d660208",
+    "url": "/react_pokedex/static/media/pokemonbg.6b36186f.png"
+  },
+  {
+    "revision": "13455ff28fc1a5dc92c77d0eb3599dc2",
+    "url": "/react_pokedex/static/media/start.13455ff2.mp4"
+  }
+]);
