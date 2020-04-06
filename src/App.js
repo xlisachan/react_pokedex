7336ef078fe240913f80af1382a5ce5dc970dce0
xlisachan <xlisachan@gmail.com>
2020-04-05 21:30:23 -0400

    Added dependencies - Redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 import Case from './components/case/Case';
 
 function App() {
   return (
-    <Case />
+    <Provider store={store}>
+      <Case />
+    </Provider>
   );
 }
 
