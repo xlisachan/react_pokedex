cfe1cf3360bfca9c565fe76f2078299cb0671eaa
xlisachan <xlisachan@gmail.com>
2020-04-12 15:19:21 -0400

    Removed lifecycle from Results component

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -4,38 +4,28 @@ import { clearResult, pokeSearch } from '../actions/searchActions';
 import { Loading, NoMatch} from './index';
 import Pokemon from './pokemon/Pokemon';
 
-class Results extends React.Component {
-  componentDidMount() {
-    if (!this.props.query) {
-      return this.props.onClearResult();
-    }
-    return this.props.onSearchPokemon(this.props.query, this.props.history);
+const Results = ({ query, history, search, onSearchPokemon = f => f }) => {
+  if (search.fetching) {
+    return <Loading />
   };
 
-  render() {
-    const { query, history, search, onSearchPokemon=f=>f } = this.props;
+  if (search.error || Object.keys(search.searchResults).length === 0) {
+    return <NoMatch query={query} />
+  };
 
-    if (search.fetching) {
-      return <Loading />
-    };
-
-    if (search.error || Object.keys(search.searchResults).length === 0) {
-      return <NoMatch query={query} />
-    };
-
-    return (
-      <Pokemon
-        query={query}
-        history={history}
-        pokemon={search.searchResults}
-        onSearchPokemon={onSearchPokemon}
-      />
-    );
-  }
+  return (
+    <Pokemon
+      query={query}
+      history={history}
+      pokemon={search.searchResults}
+      onSearchPokemon={onSearchPokemon}
+    />
+  );
 };
 
 const mapStateToProps = state => ({
-  search: state.search
+  search: state.search,
+  query: state.search.searchInput
 });
 
 const mapDispatchToProps = dispatch => ({
