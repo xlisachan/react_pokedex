cb1ea0cdc624a982d5ca96128e59b04b912aa524
xlisachan <xlisachan@gmail.com>
2021-06-25 17:01:50 -0400

    Update redux

--- src/store/index.js
+++ src/store/index.js
@@ -16,10 +16,12 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.REDUX_DEVTOOLS_EXTENSION_COMPOSE ? window.REDUX_DEVTOOLS_EXTENSION_COMPOSE({
+    // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose
   )
 );
 
-store.subscribe(saveState)
+store.subscribe(saveState);
 
 export default store;
